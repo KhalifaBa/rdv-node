2a984f4cfb96ff4a84acb4c8ab8c8facc7d4fbf0
Khalifa LO <khalifa@extra.imgt-group.com>
2026-01-09 12:08:46 +0100

    Fix CORS log debug

--- backend-express/server.js
+++ backend-express/server.js
@@ -18,11 +18,15 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    console.log("🔍 Origin reçue :", origin); 
+
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'Not allowed by CORS';
+      return callback(new Error(msg), false);
     }
+    return callback(null, true);
   },
   credentials: true
 }));
